141a642b9857fb58debb017528ae3573bfdc999b
miflexr08 <shopper.tiago@gmail.com>
2026-03-25 20:49:07 -0300

    feature: basic graphic implementation for cells

--- main.py
+++ main.py
@@ -1,9 +1,20 @@
 import pygame
+import random
 
 pygame.init()
 screen = pygame.display.set_mode(size=(1280, 720))
 clock = pygame.time.Clock()
 running = True 
+
+# Rect cell
+COLOR_BIN_BLACK = pygame.Color(0, 0, 0)
+COLOR_BIN_WHITE = pygame.Color(255, 255, 255)
+CELL_SIZE: float = 50
+x_center = (screen.get_width() / 2) - (CELL_SIZE / 2)
+y_center = (screen.get_height() / 2) - (CELL_SIZE / 2)
+cell = pygame.Rect(x_center, y_center, CELL_SIZE, CELL_SIZE)
+
+cells = [pygame.Rect(x_center, y_center, CELL_SIZE, CELL_SIZE) for _ in range(8)]
 
 
 while running:
@@ -14,7 +25,12 @@ while running:
             running = False 
     
 
-    screen.fill('purple')
+    screen.fill('white')
+    
+    color = COLOR_BIN_BLACK if random.random().__round__() == 0 else COLOR_BIN_WHITE
+    for cell in cells:
+        cell.move(50., 0.)
+        pygame.draw.rect(screen, color=color, rect=cell)
 
     print("Game is going on...")
 
